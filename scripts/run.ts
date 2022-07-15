57da22b3aba6b56432fb6d67a2659c2f55f16f89
Nicolas Lima <nicolas.pedroso2145@gmail.com>
2022-07-14 23:40:11 -0300

    My first contract :D

---
+++ scripts/run.ts
@@ -0,0 +1,22 @@
+import hre from 'hardhat';
+// Unfortunally, how we are using TypeScript, NPX does not provide the global instance hre for us
+// so we need to import it manually
+
+const main = async () => {
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy();
+  await waveContract.deployed();
+  console.log("Contract deployed to:", waveContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
