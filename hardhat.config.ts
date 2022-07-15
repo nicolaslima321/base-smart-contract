13e01a1c8ccc3bddf6b625521ea7cb507976555f
Nicolas Lima <nicolas.pedroso2145@gmail.com>
2022-07-15 00:55:57 -0300

    integrating with metamask

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+require('dotenv').config({ path: `${__dirname}/.env`})
+
 const config: HardhatUserConfig = {
   solidity: "0.8.0",
+  networks: {
+    rinkeby: {
+      url: process.env.ALCHEMY_API_URL,
+      accounts: [process.env.PRIVATE_RINKEBY_ACCOUNT_KEY ?? ''],
+    },
+  },
 };
 
 export default config;
